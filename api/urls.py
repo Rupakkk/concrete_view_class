44a325db29f50a1265842d6d3822dda375f6f334
Rupakkk <rupak.fcc@gmail.com>
2022-06-05 21:46:46 +0545

    first

--- api/urls.py
+++ api/urls.py
@@ -4,10 +4,10 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('student/',views.StudentList.as_view()),
+    path('student/',views.ListCreate.as_view()),
     # path('student/',views.StudentCreate.as_view()),
-    # path('student/<int:pk>',views.StudentRetrieve.as_view()),
-    path('student/<int:pk>',views.StudentUpdate.as_view()),
+    path('student/<int:pk>',views.UpdateRetrieveDestroy.as_view()),
+    # path('student/<int:pk>',views.StudentUpdate.as_view()),
     # path('student/<int:pk>',views.StudentDestroy.as_view()),
    
 ]
